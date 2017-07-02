5c81594a5563ad2c441321aa19e180aed5fe5b23
Sam Salefske <Sam.salefske@gmail.com>
2017-07-02 19:11:29 -0400

    Added Existing Application
    Added Existing Application

---
+++ EyeRest/Settings.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace EyeRest
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblNotificationTitle = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.txtBoxNotificationTitle = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "EyeRest";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.fileToolStripMenuItem.Text = "Show";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblNotificationTitle
+            // 
+            this.lblNotificationTitle.AutoSize = true;
+            this.lblNotificationTitle.Location = new System.Drawing.Point(26, 40);
+            this.lblNotificationTitle.Name = "lblNotificationTitle";
+            this.lblNotificationTitle.Size = new System.Drawing.Size(83, 13);
+            this.lblNotificationTitle.TabIndex = 1;
+            this.lblNotificationTitle.Text = "Notification Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Global Hotkey";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(139, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(74, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(243, 91);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(74, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "0",
+            "0 (NUMPAD)",
+            "1 (NUMPAD)",
+            "2 (NUMPAD)",
+            "3 (NUMPAD)",
+            "4 (NUMPAD)",
+            "5 (NUMPAD)",
+            "6 (NUMPAD)",
+            "7 (NUMPAD)",
+            "8 (NUMPAD)",
+            "9 (NUMPAD)"});
+            this.comboBox3.Location = new System.Drawing.Point(347, 91);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(74, 21);
+            this.comboBox3.TabIndex = 4;
+            // 
+            // txtBoxNotificationTitle
+            // 
+            this.txtBoxNotificationTitle.Location = new System.Drawing.Point(139, 40);
+            this.txtBoxNotificationTitle.Name = "txtBoxNotificationTitle";
+            this.txtBoxNotificationTitle.Size = new System.Drawing.Size(282, 20);
+            this.txtBoxNotificationTitle.TabIndex = 1;
+            this.txtBoxNotificationTitle.Text = "EyeRest";
+            this.toolTip1.SetToolTip(this.txtBoxNotificationTitle, "Enter text for notification title. Leave empty for no notification title.");
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 578);
+            this.Controls.Add(this.txtBoxNotificationTitle);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNotificationTitle);
+            this.Name = "Settings";
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblNotificationTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox txtBoxNotificationTitle;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ EyeRest/Settings.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace EyeRest
+{
+    public partial class Settings : Form
+    {
+
+        bool isActive = false;
+        Form MainForm = new Form();
+        Form another = new Form();
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsmodifiers, int vlc);
+
+        public Settings()
+        {
+            InitializeComponent();
+            //hide form to taskbar
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+            //set hotkeys
+            RegisterHotKey(this.Handle, this.GetType().GetHashCode(), 0x0004 | 0x0002, 0x48);//Set hotkey as 'ctrl + shift + h'
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312)
+            {
+                if(isActive)
+                {
+                    //disable
+                    MainForm.Hide();
+                    another.Hide();
+                    Cursor.Show();
+                    isActive = false;
+                }
+                else
+                {
+                    //get all monitors
+                    var allScreens = Screen.AllScreens;
+                    for (int i = 0; i < allScreens.Count(); i++ )
+                    {
+                        another.FormBorderStyle = FormBorderStyle.None;
+                        another.BackColor = Color.Black;
+                        another.Width = allScreens[i].Bounds.Width;
+                        another.Height = allScreens[i].Bounds.Height;
+                        another.Show();
+                        this.Location = allScreens[i].WorkingArea.Location;
+                        Cursor.Hide();
+                        isActive = true;
+                    }
+                        //foreach monitor show a new form
+                        //ShowEyeRest();
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {   
+            //GetNotificationTitle really should be pulling from AppConfig
+            notifyIcon1.BalloonTipTitle = txtBoxNotificationTitle.Text;
+
+            //GetHotkey Combination, really should be pulling from AppConfig
+            notifyIcon1.BalloonTipText = "Your Global Hot Key is: Ctrl + Shift + H";
+            notifyIcon1.ShowBalloonTip(5000);
+        }
+
+        private void ShowEyeRest()
+        {
+            MainForm.FormBorderStyle = FormBorderStyle.None;
+            MainForm.BackColor = Color.Black;
+            MainForm.Width = Screen.PrimaryScreen.Bounds.Width;
+            MainForm.Height = Screen.PrimaryScreen.Bounds.Height;
+            MainForm.Show();
+            Cursor.Hide();
+            isActive = true;
+        }
+
+        private void LoadComboboxes()
+        {
+            comboBox1.Items.Add("Alt");
+            comboBox1.Items.Add("Ctrl");
+            comboBox1.Items.Add("Shift");
+
+            comboBox2.Items.Add("Alt");
+            comboBox2.Items.Add("Ctrl");
+            comboBox2.Items.Add("Shift");
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
